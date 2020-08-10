cee129aaea6fffdbfe62ed37ad5c1f011948b13a
GrandeRojo17 <38540605+GrandeRojo17@users.noreply.github.com>
2020-08-09 21:39:26 -0500

    Updates

---
+++ precache-manifest.d78fe399c6f669d26eaf69f49fc75be6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07a932b827e0e61fe10daf9ac06aaec",
+    "url": "/GrandeRojo17/react-profile01/tree/master/reactportfolio/index.html"
+  },
+  {
+    "revision": "b045f6dbbc6688bdcddd",
+    "url": "/GrandeRojo17/react-profile01/tree/master/reactportfolio/static/css/main.95e449fc.chunk.css"
+  },
+  {
+    "revision": "580045f7f044ffdf9b86",
+    "url": "/GrandeRojo17/react-profile01/tree/master/reactportfolio/static/js/2.1705bf15.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/GrandeRojo17/react-profile01/tree/master/reactportfolio/static/js/2.1705bf15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b045f6dbbc6688bdcddd",
+    "url": "/GrandeRojo17/react-profile01/tree/master/reactportfolio/static/js/main.ea782566.chunk.js"
+  },
+  {
+    "revision": "211a9883840321bdd528",
+    "url": "/GrandeRojo17/react-profile01/tree/master/reactportfolio/static/js/runtime-main.2ac2de95.js"
+  },
+  {
+    "revision": "08565fc9fe7b06cff44f3766ef4b10c2",
+    "url": "/GrandeRojo17/react-profile01/tree/master/reactportfolio/static/media/fitnessTracker_Pic02.08565fc9.PNG"
+  },
+  {
+    "revision": "bbaa29412160f06454cc6ec916fab00b",
+    "url": "/GrandeRojo17/react-profile01/tree/master/reactportfolio/static/media/snapchat_bitmoji_clear_transparent_faceOnly_cropped.bbaa2941.png"
+  },
+  {
+    "revision": "2cdd6cee16aa368f016eec7bd3f44380",
+    "url": "/GrandeRojo17/react-profile01/tree/master/reactportfolio/static/media/weatherDash_01.2cdd6cee.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-profile01/precache-manifest.5277c97672dae0a9fe129fadec2247c1.js"
+  "/GrandeRojo17/react-profile01/tree/master/reactportfolio/precache-manifest.d78fe399c6f669d26eaf69f49fc75be6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-profile01/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GrandeRojo17/react-profile01/tree/master/reactportfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
